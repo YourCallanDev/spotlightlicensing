fbefceb5b5a172c9820265dd1d74697c538d4d4e
Callan Chesser <107786341+YourCallanDev@users.noreply.github.com>
2025-11-10 14:16:29 +0000

    Update licenseAssignment.js

--- data/licenseAssignment.js
+++ data/licenseAssignment.js
@@ -1,3 +1,9 @@
 window.licenseAssignments = [
-  { userId: 1, showId: 1, expires: "2025-12-31" }
+  { userId: 1, showId: 1, expires: "40000-12-31" },
+  { userId: 1, showId: 2, expires: "40000-12-31" },
+  { userId: 1, showId: 3, expires: "40000-12-31" },
+  { userId: 1, showId: 4, expires: "40000-12-31" },
+  { userId: 1, showId: 5, expires: "40000-12-31" },
+  { userId: 1, showId: 6, expires: "40000-12-31" },
+  { userId: 1, showId: 7, expires: "40000-12-31" },
 ];
