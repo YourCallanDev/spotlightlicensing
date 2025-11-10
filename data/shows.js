ed375ac99e54e0f80a86d0162db7c8f7b8636161
Callan Chesser <107786341+YourCallanDev@users.noreply.github.com>
2025-11-10 14:15:47 +0000

    Update shows.js

--- data/shows.js
+++ data/shows.js
@@ -15,11 +15,55 @@ window.shows = [
     id: 2,
     title: "Granny Got Stuck In The Alexa",
     description: "The second show available in our series of 'Granny Got Stuck'. This show explores Granny as she gets stuck in the Alexa!",
-    image: "assets/images/grannyalexa.jpg",
+    image: "assets/images/GGSIA Logo.png",
     files: [
-      { name: "Information Package", url: "showfiles/granny/alexa/GrannyAlexaInfo.pdf" }
-      { name: "Script", url: "showfiles/granny/alexa/GrannyAlexaScript.pdf" }
-      { name: "Audition Pieces", url: "showfiles/granny/alexa/GrannyAlexaAuditions.pdf" }
+      { name: "Information Package", url: "showfiles/Granny Got Stuck In Alexa.zip" }
     ]
-  }
+  },
+  {
+    id: 3,
+    title: "Granny Got Stuck In The TV",
+    description: "The first show available in our series of 'Granny Got Stuck'. This show explores Granny as she gets stuck in the TV!",
+    image: "assets/images/GGSIT Logo.png",
+    files: [
+      { name: "Information Package", url: "showfiles/Granny Got Stuck In The TV.zip" }
+    ]
+  },
+    {
+    id: 4,
+    title: "All Hail The Christmas Tree",
+    description: "Part of our seasonal production series, this festive comedy follows friends on a chaotic quest for the perfect Christmas tree. Along the way, they learn that the true holiday magic comes from being together, not perfection.",
+    image: "assets/images/AHCT Logo.png",
+    files: [
+      { name: "Information Package", url: "showfiles/All Hail The Christmas Tree.zip" }
+    ]
+  },
+  {
+    id: 5,
+    title: "Beach Bash Blues",
+    description: "The gang plans a perfect beach day, but everything that could go wrong does—from storms to forgotten supplies and mishaps with other beachgoers. Amid the chaos, they learn to embrace imperfection and enjoy life’s unexpected moments.",
+    image: "assets/images/BBS Logo.png",
+    files: [
+      { name: "Information Package", url: "showfiles/Beach Bash Blues.zip" }
+    ]
+  },
+    {
+    id: 6,
+    title: "The Great Garden Race",
+    description: "Part of our seasonal production series, this lively spring comedy follows a group of friends competing to create the “perfect” garden. Amid chaos, clashing styles, and flying flowerpots, they learn that beauty grows best when they work together.",
+    image: "assets/images/GGR Logo.png",
+    files: [
+      { name: "Information Package", url: "showfiles/The Great Garden Race.zip" }
+    ]
+  },
+    {
+    id: 7,
+    title: "The Harvest Festival Fiasco",
+    description: "As part of our seasonal production series, this autumn comedy sees friends battling over how to create the ideal harvest festival. Their clashing ideas turn into a heartfelt reminder that teamwork—and a little chaos—make the best celebrations.",
+    image: "assets/images/HFF Logo.png",
+    files: [
+      { name: "Information Package", url: "showfiles/The Harvest Festival Fiasco.zip" }
+    ]
+  },
+  
 ];
