0b2b4a183b6d53422aa78804140d37d2f2f0519e
Callan Chesser <107786341+YourCallanDev@users.noreply.github.com>
2025-11-10 10:40:16 +0000

    Create loginDetails.js

---
+++ data/loginDetails.js
@@ -0,0 +1,4 @@
+window.loginDetails = [
+  { id: 1, username: "callan.chesser", password: "Mabelcat1CC!!", name: "Callan Chesser" },
+  { id: 2, username: "theatreA", password: "password", name: "Theatre A" }
+];
