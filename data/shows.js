6b3b2356b9e424a85cce0c4e9c8b5908afa4014e
Callan Chesser <107786341+YourCallanDev@users.noreply.github.com>
2025-11-10 10:40:35 +0000

    Create shows.js

---
+++ data/shows.js
@@ -0,0 +1,4 @@
+window.shows = [
+  { id: 1, title: "The Phantom's Light", description: "A musical thriller.", image: "img/phantom.jpg" },
+  { id: 2, title: "Moonlight Stage", description: "A drama about fame.", image: "img/moonlight.jpg" }
+];
